f58f86891ea624a59e99b26e9a8b52606093079e
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2016-08-15 09:06:13 -0700

    start oo lesson

---
+++ oointro_demo.rb
@@ -0,0 +1,28 @@
+### OBJECT ORIENTATION INTRO ####
+  #Object Orientation allows us to setup one, generic class that will allow us to create MANY objects that we can then customize.
+  #Objects are customized by providing specific data for it's attributes.
+    #We want a username for every user. So we setup the functionality to have a username.
+    #When we create a new user, we then provide a specific username.
+
+    ##EXAMPLE##
+    #Think of facebook who has over 1.7 BILLION users. Yes. Billion. Seven of those Billon. Mind blown.
+    #There's not a fb engineer that goes making special hashes of every new users information. That would be insanity.
+    #Instead, it's better if they create a template (or blueprint or cookie cutter) that structures what a user is and what they are allowed to do.
+    #Then, as a user signs up, a new user object is created and they just need to populate the object with their specific data.
+
+
+
+### LETS CREATE THE NEXT FACEBOOK ##
+  # We are creating the next facebook and we want to create a class so that our website can scale easily to accomodate our potential 1.7 billion users.  Those users have something called attributes and methods.
+  
+  #Attributes are the nouns. They are what describe a user.
+  #Methods are the verbs. They are what a user is able to do.
+
+
+  ### PLAN BEFORE WE PROCEED ###
+  ###What attributes does a user have?
+      #(with a pair, use a whiteboard to make a list of user's attributes)
+
+
+  ###What functionality does a user have?
+      #(with a pair, use a whiteboard to make a list of user's functionality)
